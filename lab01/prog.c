443607cbd01351c193016c8de40ce0beebb12843
mu2so4 <b4.orangutang@gmail.com>
2022-09-21 11:31:47 +0700

    LAB01: replace printf() to write()

--- lab01/prog.c
+++ lab01/prog.c
@@ -1,10 +1,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 void print10lines(const char *label) {
-	for(int iteration = 0; iteration < 10; iteration++)
-		printf("%s: %d\n", label, iteration);
+	for(int iteration = 0; iteration < 10; iteration++) {
+		char str[15];
+		int count = sprintf(str, "%s: %d\n", label, iteration);
+		if(count < 0) {
+			perror("write");
+			pthread_exit(NULL);
+		}
+		write(STDOUT_FILENO, str, count);
+	}
 }
 
 void *thread_sub() {
@@ -15,14 +24,15 @@ void *thread_sub() {
 int main() {
 	pthread_attr_t attrs;
 	pthread_t thread_10;
+	int error = 1;
 	if(pthread_attr_init(&attrs) != 0) {
 		perror("pthread attr init");
-		exit(-1);
+		pthread_exit(&error);
 	}
 
 	if(pthread_create(&thread_10, &attrs, thread_sub, NULL) != 0) {
 		perror("pthread init");
-		return -1;
+		pthread_exit(&error);
 	}
 	print10lines("main");
 	//pthread_join(thread_10, NULL);
