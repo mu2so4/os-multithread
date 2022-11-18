e302ab2ce4b9008b3042e5d65e136a1d6a4ffe28
mu2so4 <b4.orangutang@gmail.com>
2022-11-18 13:08:37 +0700

    Add evaluating by blocks

--- lab09/prog.c
+++ lab09/prog.c
@@ -4,10 +4,14 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <math.h>
+
+#define MAX_THREAD_COUNT 30000
 
 typedef struct sum_args {
 	int thread_count;
 	int offset;
+	//pthread_barrier_t *barrier;
 } sum_args;
 
 typedef double res_t;
@@ -26,8 +30,13 @@ void *partical_sum(void *args) {
 	int offset = params->offset;
     
 	res_t* part_sum = calloc(1, sizeof(res_t));
-	for(long long iteration = offset; is_running; iteration += thread_count) {
-		*part_sum += 1. / (iteration * 4. + 1.) - 1. / (iteration * 4. + 3.);
+	const int block_size = 10000;
+	for(long long block_num = 0; is_running; block_num++) {
+		for(long long iteration = 0; iteration < block_size; iteration++) {
+			long long it = block_size * block_num + iteration;
+			long long member = (it * thread_count + offset) * 4 + 1;
+			*part_sum += 1. / member - 1. / (member + 2.);
+		}
 	}
 	pthread_exit((void*) part_sum);
 }
@@ -39,24 +48,34 @@ int main(int argc, char **argv) {
 		pthread_exit(NULL);
 	}
 	int thread_count = atoi(argv[1]);
+	if(thread_count > MAX_THREAD_COUNT) {
+		thread_count = MAX_THREAD_COUNT;
+		fprintf(stderr, "thread count decreased to %d\n", thread_count);
+	}
+
 	pthread_t *threads = (pthread_t*) malloc(sizeof(pthread_t) *
 			thread_count);
+
 	if(threads == NULL) {
 		perror("thread_t malloc");
 		pthread_exit(NULL);
 	}
+
 	sum_args *sargs = (sum_args*) malloc(sizeof(sum_args) * thread_count);
+
 	if(sargs == NULL) {
 		perror("sargs malloc");
 		free(threads);
 		pthread_exit(NULL);
 	}
+
 	for(int index = 0; index < thread_count; index++) {
 		sargs[index].thread_count = thread_count;
 		sargs[index].offset = index;
 	}
 
 	int true_count = thread_count;
+	//pthread_barrier_t bar;
 	for(int index = 0; index < thread_count; index++) {
 		if(pthread_create(threads + index, NULL, partical_sum,
 				sargs + index) != 0) {
@@ -81,7 +100,9 @@ int main(int argc, char **argv) {
 		}
 	}
 	result *= 4;
-	printf("\nPi=%.15f\n", result);
+	printf("\nPi=\t%.15f\n", result);
+	printf("Pi_0=\t%.15f\n", M_PI);
+	printf("d=\t%.15f\n", M_PI - result);
 
 	free(threads);
 	free(sargs);
